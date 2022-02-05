f85098cad0e5ba8a4d9e224724f8c8de9d351130
Talha Asghar <talhaasghar.contact@simplelogin.fr>
2022-02-05 12:56:06 +0500

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="whatstrending",
-    version="0.0.1",
+    version="0.0.2",
     author="Talha Asghar",
     author_email="talhaasghar.contact@simplelogin.fr",
     description="Python CLI tool to scrap latest twitter trends without using twitter api.",
